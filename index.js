46dcab5e7118a13e12c47a1be8894d9de2560b75
TajbeerAhamed <mdtajbeerahamedrimon@gmail.com>
2022-10-26 20:44:32 +0600

    fake-data-added

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ app.use(cors())
 
 const categories = require('./data/categories.json')
 
+const details = require('./data/Details.json')
+
+
+
 app.get("/", (req, res) => {
   res.send("Web api running");
 });
 
 app.get('/categories', (req,res) =>{
     res.send(categories)
+})
+
+app.get('/details', (req ,res) =>{
+  res.send(details)
 })
 
 app.listen(port, () =>{
